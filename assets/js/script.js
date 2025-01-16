a6b951a8889d7146b2caabc1be4cff5001f51bc5
Saurabh Kumar <147180839+Saurabhtbj1201@users.noreply.github.com>
2025-01-16 11:21:09 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,22 +37,16 @@ $(document).ready(function () {
         }, 500, 'linear')
     });
 
+    // EmailJS to send contact form data
+$("#contact-form").submit(function (event) {
+    event.preventDefault(); // Prevent the default form submission
+//  submit the form to Formspree as a fallback
+$("#contact-form").off("submit").submit();
+});
+});
     // <!-- emailjs to mail contact form data -->
-    $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
 
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
-            .then(function (response) {
-                console.log('SUCCESS!', response.status, response.text);
-                document.getElementById("contact-form").reset();
-                alert("Form Submitted Successfully");
-            }, function (error) {
-                console.log('FAILED...', error);
-                alert("Form Submission Failed! Try Again");
-            });
-        event.preventDefault();
-    });
-    // <!-- emailjs to mail contact form data -->
+
 
 });
 
